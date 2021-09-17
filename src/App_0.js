8df4966456fa9680a13b7136179e1722e58be263
david <rd02navc2@gmail.com>
2021-09-17 16:30:01 +0800

    kus-api-got-17-09-2021

--- src/App_0.js
+++ src/App_0.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from "react";
 
+export default App;
 
 function App() {
   const [error, setError] = useState(null);
